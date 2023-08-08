2eb8d474af2170145598552d95ea5cbced1daf2c
Ignacio Cunial <igsecu@hotmail.com>
2023-08-08 11:31:36 -0300

    Created handleGetRequest() -> Routes / and /courses completed

--- routing_nodejs.js
+++ routing_nodejs.js
@@ -2,8 +2,27 @@ const http = require("http");
 const courses = require("./courses.js");
 
 const server = http.createServer((req, res) => {
-  res.end("");
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      return handleGetRequest(req, res);
+    default:
+      console.log(`Method used is not handled by the server: ${method}`);
+  }
 });
+
+function handleGetRequest(req, res) {
+  const path = req.url;
+
+  if (path === "/") {
+    res.statusCode = 200;
+    res.end("Welcome to my first server and Api created with Node.js");
+  } else if (path === "/courses") {
+    res.statusCode = 200;
+    res.end(JSON.stringify(courses));
+  }
+}
 
 const PORT = process.env.PORT || 5000;
 
