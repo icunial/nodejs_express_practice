50927d91fcb9dff2cffe9ce215f087031a6b01e0
Ignacio Cunial <igsecu@hotmail.com>
2023-08-08 11:24:07 -0300

    Created server with http module

--- routing_nodejs.js
+++ routing_nodejs.js
@@ -1,2 +1,10 @@
 const http = require("http");
 const courses = require("./courses.js");
+
+const server = http.createServer((req, res) => {
+  res.end("");
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(`Server is listening on port: ${PORT}`);
