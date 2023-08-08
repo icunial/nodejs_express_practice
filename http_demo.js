f2b65683f3551521cd72fd7df0920a050cd07918
Ignacio Cunial <igsecu@hotmail.com>
2023-08-08 10:54:15 -0300

    res - req practice -> http_demo.js

--- http_demo.js
+++ http_demo.js
@@ -1,11 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  /* console.log("-> REQ");
+  console.log(req.url);
+  console.log(req.method);
+  console.log(req.headers); */
+
+  console.log("-> RES");
+  console.log(res.statusCode);
+  res.setHeader("content-type", "application/json");
+  console.log(res.getHeader());
   res.end("Hello World!");
 });
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}...`);
 });
